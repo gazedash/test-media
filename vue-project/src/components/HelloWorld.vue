b640d45b9131394df062922636c885eda57a828e
Your Name <you@example.com>
2024-04-15 00:02:19 +0300

    TRY WITH LAME

--- vue-project/src/components/HelloWorld.vue
+++ vue-project/src/components/HelloWorld.vue
@@ -11,13 +11,9 @@
 
     {{ format }}
 
-    <button @click="micCheck">
-        start
-    </button>
+    <button @click="micCheck">start</button>
 
-    <button @click="finish">
-        stop
-    </button>
+    <button @click="finish">stop</button>
 
     {{ JSON.stringify(options) }}
   </div>
@@ -25,19 +21,22 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import recordrtc from "recordrtc";
+import lamejs from "lamejs";
 
 let recordingDataURL = ref("");
 let format = ref("");
-let recorderRef = ref();
+let recorderRef = ref<recordrtc.RecordRTCPromisesHandler>();
 
 const options = {
-    recorderType: recordrtc.StereoAudioRecorder,
-    type: 'audio',
-    mimeType: 'audio/wav',
-    disableLogs: !import.meta.env.DEV,
-    bufferSize: 4096,
-    numberOfAudioChannels: 1,
-  }
+  recorderType: recordrtc.StereoAudioRecorder,
+  type: "audio" as const,
+  mimeType: "audio/wav" as const,
+  disableLogs: !import.meta.env.DEV,
+  bufferSize: 4096 as const,
+  numberOfAudioChannels: 1 as const,
+  sampleRate: 48000,
+  desiredSampleRate: 48000,
+};
 
 const micCheck = async () => {
   let stream = await navigator.mediaDevices.getUserMedia({
@@ -51,11 +50,41 @@ const micCheck = async () => {
 };
 
 const finish = async () => {
-await recorderRef.value.stopRecording();
-let dataURL = await recorderRef.value.getDataURL();
-recordingDataURL.value = dataURL;
+  await recorderRef.value?.stopRecording();
 
-let blob = await recorderRef.value.getBlob();
-format.value = blob.size * 0.000001 + " " + blob.type
-}
+  let channels = 1; //1 for mono or 2 for stereo
+  let sampleRate = 48000; //44.1khz (normal mp3 samplerate)
+  let kbps = 128; //encode 128kbps mp3
+  let mp3encoder = new lamejs.Mp3Encoder(channels, sampleRate, kbps);
+
+  let blobSource = await recorderRef.value?.getBlob();
+  if (blobSource) {
+
+      // samples = new Int16Array(44100); //one second of silence (get your data from the source you have)
+      let samples123 = await blobSource?.arrayBuffer(); //one second of silence (get your data from the source you have)
+      const samples = new Int16Array(samples123);
+
+    let sampleBlockSize = 1152; //can be anything but make it a multiple of 576 to make encoders life easier
+
+    let mp3Data = [];
+    for (let i = 0; i < samples.length; i += sampleBlockSize) {
+      let sampleChunk = samples.subarray(i, i + sampleBlockSize);
+      let mp3buf = mp3encoder.encodeBuffer(sampleChunk);
+      if (mp3buf.length > 0) {
+        mp3Data.push(mp3buf);
+      }
+    }
+    let mp3buf = mp3encoder.flush(); //finish writing mp3
+
+    if (mp3buf.length > 0) {
+      mp3Data.push(new Int8Array(mp3buf));
+    }
+
+    let blob = new Blob(mp3Data, { type: "audio/mp3" });
+    let url = window.URL.createObjectURL(blob);
+    console.log("MP3 URl: ", url);
+    recordingDataURL.value = url;
+    format.value = blob.size * 0.000001 + " " + blob.type;
+  }
+};
 </script>
