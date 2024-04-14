0d88dea39f92340177f7fc5503afa01993e5d09d
Your Name <you@example.com>
2024-04-14 23:23:40 +0300

    try 22k

--- vue-project/src/components/HelloWorld.vue
+++ vue-project/src/components/HelloWorld.vue
@@ -18,6 +18,8 @@
     <button @click="finish">
         stop
     </button>
+
+    {{ JSON.stringify(options) }}
   </div>
 </template>
 <script setup lang="ts">
@@ -28,21 +30,23 @@ let recordingDataURL = ref("");
 let format = ref("");
 let recorderRef = ref();
 
-const micCheck = async () => {
-  let stream = await navigator.mediaDevices.getUserMedia({
-    video: false,
-    audio: true,
-  });
-  let recorder = new recordrtc.RecordRTCPromisesHandler(stream, {
+const options = {
     recorderType: recordrtc.StereoAudioRecorder,
     type: 'audio',
     mimeType: 'audio/wav',
     disableLogs: !import.meta.env.DEV,
     bufferSize: 4096,
     numberOfAudioChannels: 1,
-    sampleRate: 44100,
-    desiredSampRate: 44100,
+    sampleRate: 22050,
+    desiredSampRate: 22050,
+  }
+
+const micCheck = async () => {
+  let stream = await navigator.mediaDevices.getUserMedia({
+    video: false,
+    audio: true,
   });
+  let recorder = new recordrtc.RecordRTCPromisesHandler(stream, options);
   recorderRef.value = recorder;
 
   recorderRef.value.startRecording();
